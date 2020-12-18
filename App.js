beab0f4d606135c3b6500e5107e68c1b8e09b217
pennd44 <pdiba@oxbowschool.org>
2020-12-17 20:11:54 -0500

    initial

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+// import Gestures from 'react-native-easy-gestures'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView >
+      <Text>Triango!</Text>
       <StatusBar style="auto" />
-    </View>
+      <Image
+        style={styles.board}
+        source={{
+          uri:
+          'https://i.pinimg.com/originals/b2/3e/d5/b23ed57ace9fea557510ac58a4a4b417.png'
+        }}
+      />
+        <Image
+          onPress={{}}
+          style={styles.logo}
+          source={{
+            uri:
+              'https://lh3.googleusercontent.com/proxy/4M35JQqUW5s6fFGcrxlRANqomXVAKoC3c9Hwttv8j8mNtKmpn317lSNMsyodK3HzQCv_3prD4TCnV-qEVT_Ipj6GvA'
+          }}
+        />
+    </SafeAreaView>
   );
 }
 
@@ -18,4 +34,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  board: {
+    width: 400,
+    height: 400,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
 });
+
